7bece7aa3aa97b63e7e64680fe98037fb5e0010a
Sagar patel <sagarpatel@Sagars-MBP.hitronhub.home>
2022-03-30 10:01:43 -0300

    Modified login screen

--- iOS-SwiftProject/iOS-SwiftProject/Login/LoginViewController.swift
+++ iOS-SwiftProject/iOS-SwiftProject/Login/LoginViewController.swift
@@ -8,7 +8,10 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    let titleLabel = UILabel()
+    let subtitleLabel = UILabel()
+    
     let loginView = LoginView()
     let signInButton = UIButton(type: .system)
     let errorLabel = UILabel()
@@ -26,13 +29,28 @@ class LoginViewController: UIViewController {
         style()
         layout()
     }
-
-
+    
+    
 }
 
 extension LoginViewController {
     
     private func style() {
+        view.backgroundColor = .systemBackground
+        
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.textAlignment = .center
+        titleLabel.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+        titleLabel.adjustsFontForContentSizeCategory = true
+        titleLabel.text = "Bankey"
+        
+        subtitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        subtitleLabel.textAlignment = .center
+        subtitleLabel.font = UIFont.preferredFont(forTextStyle: .title3)
+        subtitleLabel.adjustsFontForContentSizeCategory = true
+        subtitleLabel.numberOfLines = 0
+        subtitleLabel.text = "Your premium source for all things banking!"
+        
         loginView.translatesAutoresizingMaskIntoConstraints = false
         
         signInButton.translatesAutoresizingMaskIntoConstraints = false
@@ -49,9 +67,24 @@ extension LoginViewController {
     }
     
     private func layout() {
+        view.addSubview(titleLabel)
+        view.addSubview(subtitleLabel)
         view.addSubview(loginView)
         view.addSubview(signInButton)
         view.addSubview(errorLabel)
+        
+        // Title
+        NSLayoutConstraint.activate([
+            subtitleLabel.topAnchor.constraint(equalToSystemSpacingBelow: titleLabel.bottomAnchor, multiplier: 3),
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
+        // Subtitle
+        NSLayoutConstraint.activate([
+            loginView.topAnchor.constraint(equalToSystemSpacingBelow: subtitleLabel.bottomAnchor, multiplier: 3),
+            subtitleLabel.leadingAnchor.constraint(equalTo: loginView.leadingAnchor),
+            subtitleLabel.trailingAnchor.constraint(equalTo: loginView.trailingAnchor)
+        ])
         
         //loginView
         NSLayoutConstraint.activate([
@@ -89,7 +122,7 @@ extension LoginViewController {
             assertionFailure("Username / password should never be nil")
             return
         }
-
+        
         if username.isEmpty || password.isEmpty {
             configureView(withMessage: "Username / password cannot be blank")
             return
