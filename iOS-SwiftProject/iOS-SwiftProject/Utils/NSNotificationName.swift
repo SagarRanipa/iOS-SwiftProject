45b4f6d18da925dc193b269d62ec11650de916a4
Sagar patel <sagarpatel@Sagars-MBP.hitronhub.home>
2022-04-02 11:29:09 -0300

    feat: add logoutButton

---
+++ iOS-SwiftProject/iOS-SwiftProject/Utils/NSNotificationName.swift
@@ -0,0 +1,12 @@
+//
+//  NSNotificationName.swift
+//  iOS-SwiftProject
+//
+//  Created by Sagar patel on 2022-04-02.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let logout = Notification.Name("Logout")
+}
